4a0820413979c063d04c6b12fb958918901abc73
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 16:12:51 -0700

    Alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,7 @@
 /* Function prototype for the _putchar function */
 int _putchar(char c);
 
+/* Function prototype for the print_alphabet function */
+void print_alphabet(void);
+
 #endif /* MAIN_H */
