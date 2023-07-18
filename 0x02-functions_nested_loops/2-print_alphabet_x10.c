c1be1d050970bfb661390fc645f47d3d26c42bc0
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 16:23:03 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet
+ */
+void print_alphabet(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        _putchar(letter);
+    }
+}
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        print_alphabet();
+        _putchar('\n');
+    }
+}
