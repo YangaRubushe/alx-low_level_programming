a0b82609a4314acaa37f32b787d115dedbafb284
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 19:39:02 -0700

    Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,22 +9,22 @@
  */
 void print_times_table(int n)
 {
-	int i, j, result;
-
-	if (n > 15 || n < 0)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
+		int i, j, result;
 
-			if (j == 0)
-				printf("%d", result);
-			else
-				printf(",%3d", result);
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+
+				if (j == 0)
+					printf("%d", result);
+				else
+					printf(", %3d", result);
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
