e7fa658ea88114bd9ae3d10d8e228f236dfbb7d4
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 17:01:42 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,20 +8,8 @@
 int _islower(int c) {
   /* Check if c is between 97 and 122, inclusive. */
   if (c >= 97 && c <= 122) {
-    return (1);
+    return 1;
   } else {
-    return (0);
+    return 0;
   }
-}
-
-int main(void) {
-  int r;
-
-  r = _islower('H');
-  printf("%d\n", r);
-  r = _islower('o');
-  printf("%d\n", r);
-  r = _islower(108);
-  printf("%d\n", r);
-  return (0);
 }
