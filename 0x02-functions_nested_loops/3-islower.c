1bd66965be79080725ddb263143cb5a64cbdfa8f
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 17:09:26 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,15 @@
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower(int c) {
-  /* Check if c is between 97 and 122, inclusive. */
-  if (c >= 97 && c <= 122) {
-    return 1;
-  } else {
-    return 0;
-  }
+int _islower(int c)
+{
+    /* Check if c is between 97 and 122, inclusive. */
+    if (c >= 97 && c <= 122)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
