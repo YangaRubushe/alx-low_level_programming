1bd5a7804ba2e970a6f65e879516f06314ba4791
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 17:26:35 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h> // Include the standard library function isalpha
+
+/**
+ * _isalpha - Checks if a character is an alphabetic letter.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is an alphabetic letter, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+    return isalpha(c);
+}
