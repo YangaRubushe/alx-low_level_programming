0a2988f880eb18155b8de5ff52237194da233ea6
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 18:31:01 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - Prints the sign of a number.
@@ -11,19 +10,19 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
