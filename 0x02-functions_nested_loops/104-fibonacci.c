c232867db0a5d2b6ee58b9593226c120d3ca683d
YangaRubushe <zilithemba10@gmail.com>
2023-07-19 13:55:40 -0700

    Fibonnaci to steal passwords

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,83 +1,52 @@
 #include <stdio.h>
 
-#define DIGITS 1000
-
 /**
- * print_number - Print a number represented as an array of digits
- * @num: Pointer to the array of digits
- * @size: Size of the array (number of digits)
- */
-void print_number(int *num, int size)
-{
-	int i;
-
-	for (i = size - 1; i >= 0; i--)
-		printf("%d", num[i]);
-}
-
-/**
- * add_numbers - Add two numbers represented as arrays of digits
- * @num1: Pointer to the first number's array of digits
- * @num2: Pointer to the second number's array of digits
- * @size1: Size of the first array (number of digits)
- * @size2: Size of the second array (number of digits)
- * @result: Pointer to store the sum of the two numbers as an array of digits
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
  *
- * Return: The size of the result array (number of digits)
+ * Return: Always 0.
  */
-int add_numbers(int *num1, int *num2, int size1, int size2, int *result)
-{
-	int carry = 0, sum, i;
 
-	for (i = 0; i < size1 || i < size2 || carry; i++)
-	{
-		sum = carry;
-		if (i < size1)
-			sum += num1[i];
-		if (i < size2)
-			sum += num2[i];
-
-		result[i] = sum % 10;
-		carry = sum / 10;
-	}
-
-	return (i);
-}
-
-/**
- * main - Entry point
- *
- * Description: Finds and prints the first 98 Fibonacci numbers
- * starting with 1 and 2, separated by commas and spaces.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int num1[DIGITS] = {0}, num2[DIGITS] = {0}, temp[DIGITS] = {0};
-	int size1 = 1, size2 = 1, temp_size, i;
+	int c;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	num1[0] = 1;
-	num2[0] = 2;
-
-	printf("%d, %d", num1[0], num2[0]);
-
-	for (i = 2; i < 98; i++)
+	for (c = 0; c < 92; c++)
 	{
-		temp_size = add_numbers(num1, num2, size1, size2, temp);
-		printf(", ");
-		print_number(temp, temp_size);
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
 
-		for (int j = 0; j < temp_size; j++)
-		{
-			num1[j] = num2[j];
-			num2[j] = temp[j];
-		}
-		size1 = size2;
-		size2 = temp_size;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	printf("\n");
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
+	for (c = 93; c < 99; c++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (c != 98)
+			printf(", ");
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
