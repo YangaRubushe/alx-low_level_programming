5d07cf357b2caaafb89e18a60d937a37b7600cbc
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 18:26:24 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,16 +7,24 @@ int _putchar(char c);
 /* Function prototype for the print_alphabet function */
 void print_alphabet(void);
 
-/* Function prototype for the print_alphabet_x10 function */
-void print_alphabet_x10(void);
-
-/* Function prototype for the _islower function */
 int _islower(int c);
 
-/* Function prototype for the _isalpha function */
 int _isalpha(int c);
 
-/* Function prototype for the print_sign function */
-int print_sign(int n)
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
 
 #endif /* MAIN_H */
