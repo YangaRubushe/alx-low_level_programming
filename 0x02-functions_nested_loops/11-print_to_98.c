a5a45b455a6f90f475f247f714acab51fbaba395
YangaRubushe <zilithemba10@gmail.com>
2023-07-18 19:14:27 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
@@ -8,20 +9,12 @@
  */
 void print_to_98(int n)
 {
-	/* Print the starting number */
-	printf("%d", n);
-
-	/* Determine the direction of the loop (increment or decrement) */
 	int step = n <= 98 ? 1 : -1;
 
-	/* Loop until we reach 98 */
 	while (n != 98)
 	{
-		/* Print the next number separated by a comma and a space */
-		printf(", %d", n += step);
+		printf("%d, ", n);
+		n += step;
 	}
-
-	/* Print a new line after the loop */
-	printf("\n");
+	printf("%d\n", n);
 }
-
