d62f7e0f3eb4a59710d9ecdc94f1a1a153e8efda
YangaRubushe <zilithemba10@gmail.com>
2023-08-02 15:02:32 -0700

    Main

---
+++ 0-puts_recursion.c/main.h
@@ -0,0 +1,47 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Declaration of the _putchar function */
+int _putchar(char c);
+
+/* Declaration of the _puts_recursion function */
+void _puts_recursion(char *s);
+
+/* Declaration of the _print_rev_recursion function */
+void _print_rev_recursion(char *s);
+
+/* Declaration of the _strlen_recursion function */
+int _strlen_recursion(char *s);
+
+/* Declaration of the factorial function */
+int factorial(int n);
+
+/* Declaration of the _pow_recursion function */
+int _pow_recursion(int x, int y);
+
+/* Helper function for _sqrt_recursion to find the square root */
+int sqqrt(float n, float l, float r);
+
+/* Declaration of the _sqrt_recursion function */
+int _sqrt_recursion(int n);
+
+/* Helper function for is_prime_number to check for primes */
+int prime(float n, float x);
+
+/* Declaration of the is_prime_number function */
+int is_prime_number(int n);
+
+/* Helper function for is_palindrome to check for palindromes */
+int pal(char *s, int i, int j);
+
+/* Declaration of the is_palindrome function */
+int is_palindrome(char *s);
+
+/* Helper function for _sl to find string length */
+int _sl(char *s);
+
+/* Declaration of the wildcmp function */
+int wildcmp(char *s1, char *s2);
+
+#endif
+
