3208b2442544742099a76ef5b2b9f283174803e1
YangaRubushe <zilithemba10@gmail.com>
2023-08-10 15:35:16 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-
+	
 	for (unsigned int i = 0; i < old_size; i++)
 		*((char *)new_ptr + i) = *((char *)ptr + i);
 
