d1b0ced1965f0d654a1bd41a21e1e24749c97cac
YangaRubushe <zilithemba@10>
2023-08-29 05:52:45 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		current = *head;
-		*head = (*head)->next; 
-		free(current);
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
+
+	head = NULL;
 }
